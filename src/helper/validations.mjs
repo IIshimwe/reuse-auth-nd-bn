bec54b320c134fdeddd9a543b02f62f8461ace78
IIshimwe <ixacson@gmail.com>
2024-05-09 00:06:31 +0200

    Add user validation method

---
+++ src/helper/validations.mjs
@@ -0,0 +1,12 @@
+import Joi from "joi";
+
+export const validateUser = (user) => {
+  const schema = Joi.object({
+    firstName: Joi.string().min(3).max(100).required(),
+    lastName: Joi.string().min(3).max(100).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required(),
+  });
+
+  return schema.validate(user);
+};
