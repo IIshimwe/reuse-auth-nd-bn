a9d709554e2e039b41e2dc651632c7aeaacfba94
IIshimwe <ixacson@gmail.com>
2024-05-16 23:32:14 +0200

    Authorize an authenticated user to view all users

--- src/routers/user.router.mjs
+++ src/routers/user.router.mjs
@@ -1,7 +1,8 @@
+import { auth } from "../middlewares/auth.mjs";
 import { createuser, getUsers } from "../controllers/user.controller.mjs";
 import express from "express";
 const router = express.Router();
 
 router.post("/", createuser);
-router.get("/", getUsers);
+router.get("/", auth, getUsers);
 export default router;
