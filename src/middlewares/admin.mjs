2cffe4be72e74601ac1fe894bec216ac9af6506d
IIshimwe <ixacson@gmail.com>
2024-05-19 21:34:26 +0200

    Fix: Request admin user

--- src/middlewares/admin.mjs
+++ src/middlewares/admin.mjs
@@ -1,5 +1,5 @@
 export const isAdmin = async (req, res, next) => {
-  if (req.user.role === "isAdmin") {
+  if (req.user.isAdmin === "isAdmin") {
     next();
   } else {
     res.status(401).json({
