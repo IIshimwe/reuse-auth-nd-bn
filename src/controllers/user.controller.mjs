b5f7d21d8dc01d7053393baa49bbab881129c38f
IIshimwe <ixacson@gmail.com>
2024-05-24 23:25:18 +0200

    Send registration email on the user signup

--- src/controllers/user.controller.mjs
+++ src/controllers/user.controller.mjs
@@ -2,24 +2,47 @@ import User from "../../config/models/user.model.mjs";
 import { validateUser } from "../helpers/validations.mjs";
 import { hashPassword } from "../helpers/helper.mjs";
 import _ from "lodash";
+import { sendMail } from "../helpers/sendMail.mjs";
 
 export const createuser = async (req, res) => {
-  const { error } = validateUser(req.body);
-  if (error) return res.status(400).json({ error: error.details[0].message });
+  try {
+    const { error } = validateUser(req.body);
+    if (error) return res.status(400).json({ error: error.details[0].message });
 
-  let user = await User.findOne({ email: req.body.email });
-  if (user) return res.status(400).json({ error: "User already exists" });
+    let user = await User.findOne({ email: req.body.email });
+    if (user) return res.status(400).json({ error: "User already exists" });
 
-  user = new User(
-    _.pick(req.body, ["firstName", "lastName", "email", "password"])
-  );
-  user.password = await hashPassword(user.password);
-  await user.save();
+    user = new User(
+      _.pick(req.body, ["firstName", "lastName", "email", "password"])
+    );
+    user.password = await hashPassword(user.password);
 
-  res.json({
-    message: "User created successfully",
-    user: _.pick(user, ["firstName", "lastName", "email"]),
-  });
+    // Notify user that he/she has registered on the system
+
+    const subject = "Successfully registered on auth reuse";
+    const message = `
+      Hi there,
+      <br />
+      Thank you for signing up for Auth Reuse.
+      <br />
+      <br />If you did not sign up for on this platform, you can safely ignore this email.
+      Best,
+      <br /><br />
+      Isaac ISHIMWE - SWE
+    `;
+
+    await sendMail(req.body.email, subject, message);
+
+    await user.save();
+
+    res.json({
+      message: "User created successfully",
+      user: _.pick(user, ["firstName", "lastName", "email"]),
+    });
+  } catch (error) {
+    console.log("Error creating user account: ", error);
+    return res.status(500).json({ error: error.message });
+  }
 };
 
 export const getUsers = async (req, res) => {
