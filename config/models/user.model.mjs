57afaad0bd286e2fd3d0cb603a86f91f52801a5e
IIshimwe <ixacson@gmail.com>
2024-06-01 23:54:18 +0200

    Add profile image field

--- config/models/user.model.mjs
+++ config/models/user.model.mjs
@@ -14,6 +14,10 @@ const userSchema = new Schema({
     maxlength: 100,
     required: true,
   },
+  profile_image: {
+    public_id: { type: String, required: true },
+    url: { type: String, required: true },
+  },
   email: {
     type: String,
     required: true,
